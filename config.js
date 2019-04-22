0d0e99d570b32e7ce3055b47658eb5088e382433
nibble4bits <andiyasha16@gmail.com>
2019-04-22 17:56:16 -0500

    Added new config variables

--- config.js
+++ config.js
@@ -4,4 +4,7 @@ module.exports = {
         fromEmail: process.env.FROM_EMAIL || 'master.programmer.delasalle@gmail.com',
         password: process.env.EMAIL_PASSWORD || 'Programacion1'
     },
+    database: {
+        connectionString: process.env.MONGODB_URI || 'mongodb://localhost:27017/appEmpresariales'
+    }
 };
