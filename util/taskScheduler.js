ed369e2f9669dffd4f2f1e5fded167c29a339392
nibble4bits <andiyasha16@gmail.com>
2019-04-22 23:02:44 -0500

    Added the possibility to schedule tasks

---
+++ util/taskScheduler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const cron = require('node-cron');
+
+const taskScheduler = {
+    /**
+     * Esta función llama a una función que se ejecutará una sola vez dentro de 10 minutos
+     * @param {Function} taskToRun La función que está programada para ejecutarse en 10 minutos
+     */
+    scheduleTaskAndRunOnceIn10Minutes: function (taskToRun) {
+        let task = new cron.schedule('* */10 * * * *', () => {
+            taskToRun();
+            task.destroy();
+        });
+    
+        task.start();
+    }
+};
+
+module.exports = taskScheduler;
