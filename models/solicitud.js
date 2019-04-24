eb0a7e43078e3b5fc04beb169ea8859ac7ee3c5b
nibble4bits <andiyasha16@gmail.com>
2019-04-23 21:59:54 -0500

    Created Solicitud Model

---
+++ models/solicitud.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment-reworked').MongooseAutoIncrementID;
+
+const SolicitudSchema = new mongoose.Schema({
+    IdUsuario: Number,
+    IdUsuarioIT: Number,
+    FechaCreacion: Date,
+    FechaEnProceso: { type: Date, default: null },
+    FechaTerminado: { type: Date, default: null },
+    Razon: String,
+    ComentariosIT: Array,
+    RutaDocumento: { type: String, default: null },
+    Prioridad: { type: Number, min: 0, max: 2 } // 0 - BAJA, 1 - MEDIA, 2 - ALTA
+});
+
+// Agregamos el plugin que permite que cada _id sea autoincrementable y numérico
+SolicitudSchema.plugin(autoIncrement.plugin, { modelName: 'Solicitud' });
+
+module.exports = {
+    modeloSolicitud: mongoose.model('Solicitud', SolicitudSchema),
+    prioridad: {
+        BAJA: 0,
+        MEDIA: 1,
+        ALTA: 2
+    }
+};
