6d69b1176171bbcf81e42e667dfd957d134ab819
nibble4bits <andiyasha16@gmail.com>
2019-04-23 22:01:52 -0500

    Created JWT Auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const hsc = require('http-status-codes');
+
+const TOKEN = require('../util/token');
+
+function isAuth(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(hsc.UNAUTHORIZED).send({ respuesta: 'No tiene autorización para acceder al recurso solicitado' });
+    }
+
+    const jwt = req.headers.authorization.split(' ')[1];
+    const decodedJWT = TOKEN.validateToken(jwt);
+
+    if (decodedJWT) {
+        next();
+    }
+    else {
+        res.status(hsc.UNAUTHORIZED).send({ respuesta: 'Su token ha expirado o es inválido' });
+    }
+}
+
+module.exports = isAuth
