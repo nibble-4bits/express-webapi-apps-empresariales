d1db5984bcb675654fe9c0318d270b1e898ea039
nibble4bits <andiyasha16@gmail.com>
2019-04-25 23:22:23 -0500

    Added routes for IT User and IT Manager

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const CONFIG = require('../config');
 const LOGIN = require('../controllers/login');
 const COMMON_USER = require('../controllers/commonUser');
 const IT_USER = require('../controllers/ITUser');
+const MANAGER_USER = require('../controllers/gerenteITUser');
 const AUTH = require('../middleware/auth');
 
 const route = express.Router();
@@ -17,10 +18,18 @@ route.post('/passwordRecovery', LOGIN.passwordRecovery);
 route.get('/activateAccount/:usuarioId/:codigoSecreto', LOGIN.activateAccount)
 
 /* RUTAS USUARIO COMUN */
-route.get('/commonUser/getSolicitudes/:idUsuario'/*, AUTH*/, COMMON_USER.getAllSolicitudes);
+route.get('/commonUser/getMySolicitudes/:idUsuario'/*, AUTH*/, COMMON_USER.getAllMySolicitudes);
 route.post('/commonUser/sendSolicitud'/*, AUTH*/, COMMON_USER.addNewSolicitud);
 
 /* RUTAS USUARIO IT */
+route.get('/itUser/getUnattendedSolicitudes', IT_USER.getAllUnattendedSolicitudes);
+route.get('/itUser/getMySolicitudes/:idUsuarioIT', IT_USER.getAllMySolicitudes);
 route.get('/itUser/getDocument/:idSolicitud', IT_USER.getDocument);
+
+/* RUTAS USUARIO GERENTE IT */
+route.get('/managerUser/getSolicitudesByCommonUser/:nombre/:apellidos', MANAGER_USER.getSolicitudesByCommonUser);
+route.get('/managerUser/getSolicitudesByITUser/:nombre/:apellidos', MANAGER_USER.getSolicitudesByITUser);
+route.get('/managerUser/getSolicitudesByDateRange/:startDate/:endDate', MANAGER_USER.getSolicitudesByDateRange);
+route.get('/managerUser/getSolicitudById/:idSolicitud', MANAGER_USER.getSolicitudesByCommonUser);
 
 module.exports = route;
