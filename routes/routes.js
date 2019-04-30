fdf1d0c9ba93722ee10882c5228b3a79310d0ac3
nibble4bits <andiyasha16@gmail.com>
2019-04-30 00:07:34 -0500

    Finished required IT User routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 
-const CONFIG = require('../config');
 const LOGIN = require('../controllers/login');
 const COMMON_USER = require('../controllers/commonUser');
 const IT_USER = require('../controllers/ITUser');
@@ -18,17 +17,21 @@ route.post('/passwordRecovery', LOGIN.passwordRecovery);
 route.get('/activateAccount/:usuarioId/:codigoSecreto', LOGIN.activateAccount)
 
 /* RUTAS USUARIO COMUN */
-route.get('/commonUser/getMySolicitudes/:idUsuario'/*, AUTH*/, COMMON_USER.getAllMySolicitudes);
-route.post('/commonUser/sendSolicitud'/*, AUTH*/, COMMON_USER.addNewSolicitud);
+route.get('/commonUser/getMySolicitudes/:idUsuario', AUTH, COMMON_USER.getAllMySolicitudes);
+route.post('/commonUser/sendSolicitud', AUTH, COMMON_USER.addNewSolicitud);
 
 /* RUTAS USUARIO IT */
-route.get('/itUser/getUnattendedSolicitudes', IT_USER.getAllUnattendedSolicitudes);
-route.get('/itUser/getMySolicitudes/:idUsuarioIT', IT_USER.getAllMySolicitudes);
+route.get('/itUser/getUnattendedSolicitudes', AUTH, IT_USER.getAllUnattendedSolicitudes);
+route.get('/itUser/getMySolicitudes/:idUsuarioIT', AUTH, IT_USER.getAllMySolicitudes);
+route.post('/itUser/assignSolicitud', AUTH, IT_USER.assignSolicitud);
+route.post('/itUser/addCommentSolicitud', AUTH, IT_USER.addCommentToSolicitud);
+route.post('/itUser/assignFechaEnProceso', AUTH, IT_USER.assignFechaEnProceso);
+route.post('/itUser/assignFechaTerminado', AUTH, IT_USER.assignFechaTerminado);
 
 /* RUTAS USUARIO GERENTE IT */
-route.get('/managerUser/getSolicitudesByCommonUser/:nombre/:apellidos', MANAGER_USER.getSolicitudesByCommonUser);
-route.get('/managerUser/getSolicitudesByITUser/:nombre/:apellidos', MANAGER_USER.getSolicitudesByITUser);
-route.get('/managerUser/getSolicitudesByDateRange/:startDate/:endDate', MANAGER_USER.getSolicitudesByDateRange);
-route.get('/managerUser/getSolicitudById/:idSolicitud', MANAGER_USER.getSolicitudById);
+route.get('/managerUser/getSolicitudesByCommonUser/:nombre/:apellidos', AUTH, MANAGER_USER.getSolicitudesByCommonUser);
+route.get('/managerUser/getSolicitudesByITUser/:nombre/:apellidos', AUTH, MANAGER_USER.getSolicitudesByITUser);
+route.get('/managerUser/getSolicitudesByDateRange/:startDate/:endDate', AUTH, MANAGER_USER.getSolicitudesByDateRange);
+route.get('/managerUser/getSolicitudById/:idSolicitud', AUTH, MANAGER_USER.getSolicitudById);
 
 module.exports = route;
