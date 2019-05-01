68b868c52e53139e40f567e35d9809c158a460e5
nibble4bits <andiyasha16@gmail.com>
2019-05-01 00:01:00 -0500

    Send email to IT staff notifying new notification

--- controllers/commonUser.js
+++ controllers/commonUser.js
@@ -2,8 +2,10 @@
 
 const hsc = require('http-status-codes');
 
+const USUARIO = require('../models/usuario');
 const SOLICITUD = require('../models/solicitud');
 const FILE_UPLOAD = require('../util/fileUpload');
+const MAIL = require('../util/mail');
 const ERROR = require('../util/error');
 
 const commonUserController = {
@@ -33,6 +35,14 @@ const commonUserController = {
             Prioridad: req.body.prioridad
         });
 
+        let emailsITStaff = [];
+        USUARIO.modeloUsuario.find({ $or: [{ Tipo: USUARIO.tipoUsuario.IT }, { Tipo: USUARIO.tipoUsuario.IT_MANAGER }] }, 
+            (err, queryResult) => {
+                for (const usuario of queryResult) {
+                    emailsITStaff.push(usuario.Email);
+                }
+        });
+
         nuevaSolicitud.save((err, solicitudStored) => {
             if (err) {
                 return ERROR.sendErrorResponse(res,
@@ -40,6 +50,15 @@ const commonUserController = {
                     `Error al guardar solicitud en la base de datos: ${err}`);
             }
 
+            // Se envía un correo a todo el personal de IT notificando la creación de una nueva solicitud
+            MAIL.sendMailToManyHTML(emailsITStaff, 
+                `El usuario ${solicitudStored.IdUsuario} ha creado una nueva solicitud`,
+                `<h1>Información de la solicitud</h1>
+                <p><b>Fecha de creación: </b>${new Date(solicitudStored.FechaCreacion).toLocaleString()}</p>
+                <p><b>Razón: </b>${solicitudStored.Razon}</p>
+                <p><b>Ruta del documento: </b>
+                ${solicitudStored.RutaDocumento ? solicitudStored.RutaDocumento : 'No se subió ningún documento'}</p>
+                <p><b>Prioridad: </b>${SOLICITUD.getPrioridadString(solicitudStored.Prioridad)}</p>`);
             res.status(hsc.CREATED).json({ solicitud: solicitudStored });
         });
     }
