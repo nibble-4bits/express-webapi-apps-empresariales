9258157c284f3e1c069639771edfa6f3d681337d
nibble4bits <andiyasha16@gmail.com>
2019-05-01 13:05:40 -0500

    Allow Authorization header

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const app = express();
 
 app.use('/docs', express.static('repo_docs'));
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 app.use(bodyParser.json());
