8593af5800ec6866ef10e43790834c4d925da47f
nibble4bits <andiyasha16@gmail.com>
2019-05-01 13:00:01 -0500

    Allow CORS

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ const INITIALIZE = require('./initialize');
 const app = express();
 
 app.use('/docs', express.static('repo_docs'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json());
 app.use(fileUpload());
 
